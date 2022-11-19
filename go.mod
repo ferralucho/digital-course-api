6f64a8389ebefe30a8ce21819b2b3a5afd67c5ea
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-19 16:16:10 -0300

    interfaces and mocks

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/Masterminds/squirrel v1.5.3
 	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/ilyakaznacheev/cleanenv v1.4.0
 	github.com/jackc/pgx/v4 v4.10.1
---
+++ internal/usecase/interfaces.go
@@ -0,0 +1,29 @@
+// Package usecase implements application business logic. Each logic group in own file.
+package usecase
+
+import (
+	"context"
+
+	"github.com/ferralucho/digital-course-api/internal/entity"
+)
+
+//go:generate mockgen -source=interfaces.go -destination=./mocks_test.go -package=usecase_test
+
+type (
+	// CoursePlanning -.
+	CoursePlanning interface {
+		OrderCoursePlanning(context.Context, entity.CoursePlanning) (entity.UserOrderedCourse, error)
+		CoursePlanning(context.Context) ([]entity.UserOrderedCourse, error)
+	}
+
+	// CoursePlanningRepo -.
+	CoursePlanningRepo interface {
+		Store(context.Context, entity.UserOrderedCourse) error
+		GetCoursePlanning(context.Context) ([]entity.UserOrderedCourse, error)
+	}
+
+	// CoursePlanningWebAPI -.
+	CoursePlanningWebAPI interface {
+		OrderCoursePlanning(entity.CoursePlanning) (entity.CoursePlanning, error)
+	}
+)

5fda567469c7159481401aa75cdb3699468bda23
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-20 19:53:55 -0300

    user course table

--- migrations/20221120141223_course_planning.up.sql
+++ migrations/20221120141223_course_planning.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_course_planning(
-    user_id uuid,
-    course_order INTEGER
+    user_id uuid NOT NULL,
+    course_order INTEGER NOT NULL,
     course_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id, course_order)
+    CONSTRAINT user_course_planning_pkey PRIMARY KEY (user_id, course_order)
 );

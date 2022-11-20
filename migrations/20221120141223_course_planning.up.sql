431fecd51aae3d420295552ed0fb0580f9350f4e
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-20 15:06:20 -0300

    remove default

--- migrations/20221120141223_course_planning.up.sql
+++ migrations/20221120141223_course_planning.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_course_planning(
-    user_id uuid DEFAULT uuid_generate_v4 (),
+    user_id uuid,
     course_order INTEGER
     course_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, course_order)
